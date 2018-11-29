9eb64f3ce7c799c0838e7e9f0bcf309feccda393
vladimirkarabanov <karabanov.vladimir@gmail.com>
2018-11-30 00:12:21 +0200

    update

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='privat24-autoclient-sdk-python',
+    name='privat24autoclientsdk-python',
     version='1.0',
     description='Privat24 Python SDK',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['requests'],
-    # py_modules=['privat24-autoclient-sdk.py'],
 )
